38752b0d8170c1f39473a18f2d168b09d07171f4
Hexanima <nicolasfilippelli22@gmail.com>
2024-10-06 22:38:47 -0300

    Added test handler to app

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,9 +1,14 @@
-import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
+import dotenv from "dotenv";
 import { PORT } from "./constants";
 
+dotenv.config();
+
 const app = express();
+
+app.get("/*", (req, res) => {
+  res.json(req.url);
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
